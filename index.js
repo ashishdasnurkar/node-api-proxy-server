65450a3379c4eaf518f570918e49093ce8252e8e
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-12-02 21:31:16 +1100

    simple get api

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.get("/api", (req, res) => {
+  res.json({ success: true });
+});
+
 app.use(cors());
 
 app.listen(PORT, () => console.log("Listening on 5000 ..."));
